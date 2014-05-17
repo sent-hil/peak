eea65653506b6270b5e81f4acdba85df09c93043
sent-hil <me@sent-hil.com>
2014-05-17 02:31:35 -0700

    make more rubyish

--- lib/peak/core_ext/enumerable.rb
+++ lib/peak/core_ext/enumerable.rb
@@ -1,20 +1,20 @@
 # Imported from http://stackoverflow.com/questions/7749568/how-can-i-do-standard-deviation-in-ruby
 module Enumerable
   def sum
-    self.inject(0){|accum, i| accum + i }
+    inject(0){|accum, i| accum + i }
   end
 
   def mean
-    self.sum/self.length.to_f
+    sum/length.to_f
   end
 
   def sample_variance
-    m = self.mean
-    sum = self.inject(0){|accum, i| accum +(i-m)**2 }
-    sum/(self.length - 1).to_f
+    m = mean
+    sum = inject(0){|accum, i| accum + (i-m)**2 }
+    sum/(length - 1).to_f
   end
 
   def std_dev
-    return Math.sqrt(self.sample_variance)
+    Math.sqrt(sample_variance)
   end
 end
