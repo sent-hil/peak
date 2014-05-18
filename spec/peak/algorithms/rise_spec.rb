2541208bf601e9010ec2e9c8a1955bb90b74d57a
sent-hil <me@sent-hil.com>
2014-05-17 21:35:54 -0700

    add spec for 0 std dev

--- spec/peak/algorithms/rise_spec.rb
+++ spec/peak/algorithms/rise_spec.rb
@@ -27,4 +27,9 @@ describe Peak::Algorithms do
     resp = subject.call [1,1,1,1,1]
     resp.result.should == false
   end
+
+  it 'returns true if no std dev and last entry is not same' do
+    resp = subject.call [1,1,1,1,2]
+    resp.result.should == true
+  end
 end
